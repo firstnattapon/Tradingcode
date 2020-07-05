4490662e41795be91acf8bbdf3c652b255282059
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-06 00:36:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,16 @@ class Run_model :
 
 if __name__ == '__main__':
     if st.checkbox('inverse(ผกผัน)'):
-        Asset_prices   = st.number_input('Asset_prices',min_value=0.0,max_value=10000.0,value=0.0,step=0.1,format='%f')
+        Asset_prices   = st.number_input('Asset_prices',min_value=0.0,max_value=10000.0,value=0.0,step=0.1,format='%f') 
         capital_inverse = st.sidebar.number_input('capital_inverse(ผกผัน)',min_value=0.0,max_value=10000.0,value=1000.0,step=0.1,format='%f')
         inverse         = Run_model(capital=capital_inverse)
         upper_inverse   = st.sidebar.number_input('upper_inverse(ผกผัน) ',min_value=0.0,max_value=10000.0,value=100.0,step=0.1,format='%f')        
         lowwer_inverse  = st.sidebar.number_input('lowwer_inverse(ผกผัน)',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')        
         _               = inverse.inverse(upper=upper_inverse ,lowwer=lowwer_inverse)
+        
+        x_coordinates = [0 , upper_inverse] ; y_coordinates = [capital_inverse , 0]
+        plt.plot(x_coordinates, y_coordinates)
+        
         st.sidebar.text('-'*40)
         
     if st.checkbox('Direct(ผันตรง)'):
