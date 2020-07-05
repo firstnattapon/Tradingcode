a92147f31993eae67cd3ce8c61aa2c299f90c01d
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-06 02:41:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,17 @@ class Run_model :
     def __init__(self,capital):
         self.capital = capital
         
-    def inverse (self,upper=100.0 ,lowwer=0.0 ,Asset_prices=0 ):
+    def inverse (self,upper=100.0 ,lowwer=0.0):
         x1 = lowwer ;  y1 = self.capital
         x2 = upper  ;  y2 = 0
+        Asset_prices    = st.number_input('Asset_prices', min_value=0.0 ,max_value= upper_inverse ,value=0.0,step=0.1,format='%f')
         slope,b,_,_,_ = linregress([x1,x2],[y1,y2])
         x = np.arange(x1 , x2 , 0.01)
         y = (slope * x) + b
         plt.figure(figsize=(12,8))
         Port_value = slope *(Asset_prices) + b
         x_capital  = [0 , Asset_prices] ; y_capital = [Port_value , Port_value]
-        x_asset    = [0  , 0] ; y_asset = [0 , Port_value]
+        x_asset    = [Asset_prices  , Asset_prices] ; y_asset = [Asset_prices , Port_value]
         plt.plot(x , y)
         if Asset_prices != 0:
             plt.plot(x_capital, y_capital , color='r')
@@ -50,8 +51,7 @@ if __name__ == '__main__':
         inverse         = Run_model(capital=capital_inverse)
         upper_inverse   = st.sidebar.number_input('upper_inverse(ผกผัน) ',min_value=0.0,max_value=10000.0,value=100.0,step=0.1,format='%f')        
         lowwer_inverse  = st.sidebar.number_input('lowwer_inverse(ผกผัน)',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')
-        Asset_prices    = st.number_input('Asset_prices', min_value=0.0 ,max_value= upper_inverse ,value=0.0,step=0.1,format='%f')
-        _               = inverse.inverse(upper=upper_inverse ,lowwer=lowwer_inverse , Asset_prices=Asset_prices)
+        _               = inverse.inverse(upper=upper_inverse ,lowwer=lowwer_inverse)
         st.sidebar.text('-'*40)
         
     if st.checkbox('Direct(ผันตรง)'):
