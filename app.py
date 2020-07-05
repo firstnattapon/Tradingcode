089fb51389c89092869c934929949be80340baa8
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-06 02:30:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,8 @@ class Run_model :
         if Asset_prices != 0:
             plt.plot(x_capital, y_capital , color='r')
             plt.plot(x_asset , y_asset , color='r')
-            
-        plt.title('inverse(ผกผัน)',fontsize=24)
         plt.xlabel('Asset_prices',fontsize=14)
         plt.ylabel('Port_value',fontsize=14)
-        plt.tick_params(axis='both',labelsize=14)
         st.pyplot()
         st.write('Formula')
         st.write('Port_value = ' , round(slope,4) ,'*(Asset_prices) +', round(b  , 4))
