a267f0940b7fa3cbfd01f0218d8f5ede0c567bc3
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-06 06:12:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
         lowwer_inverse  = st.sidebar.number_input('lowwer_inverse(โซนล่าง)',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')
         try:
             Asset_prices = st.number_input('Asset_prices', min_value=lowwer_inverse,max_value= upper_inverse ,value=50.0,step=0.1,format='%f')
-         except:
+        except:
             pass
         _               = inverse.inverse(upper=upper_inverse ,lowwer=lowwer_inverse , Asset_prices=Asset_prices)
         st.sidebar.text('-'*40)
