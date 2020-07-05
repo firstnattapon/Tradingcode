6f2faaf7d174ebcc754431108476a829d936deb7
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-06 04:43:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,19 +60,19 @@ class Run_model :
     
 if __name__ == '__main__':
     if st.checkbox('inverse(ผกผัน)'):
-        capital_inverse = st.sidebar.number_input('capital_inverse(ผกผัน)',min_value=0.0,max_value=10000.0,value=1000.0,step=0.1,format='%f')   
+        capital_inverse = st.sidebar.number_input('capital_inverse(เงินทุนเริ่มต้น)',min_value=0.0,max_value=10000.0,value=1000.0,step=0.1,format='%f')   
         inverse         = Run_model(capital=capital_inverse)
-        upper_inverse   = st.sidebar.number_input('upper_inverse(ผกผัน) ',min_value=0.0,max_value=10000.0,value=100.0,step=0.1,format='%f')        
-        lowwer_inverse  = st.sidebar.number_input('lowwer_inverse(ผกผัน)',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')
+        upper_inverse   = st.sidebar.number_input('upper_inverse(โซนบน) ',min_value=0.0,max_value=10000.0,value=100.0,step=0.1,format='%f')        
+        lowwer_inverse  = st.sidebar.number_input('lowwer_inverse(โซนล่าง)',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')
         Asset_prices    = st.number_input('Asset_prices', min_value=lowwer_inverse ,max_value= upper_inverse ,value=50.0,step=0.1,format='%f')
         _               = inverse.inverse(upper=upper_inverse ,lowwer=lowwer_inverse , Asset_prices=Asset_prices)
         st.sidebar.text('-'*40)
         
     if st.checkbox('Direct(ผันตรง)'):
-        capital_Direct  = st.sidebar.number_input('capital_Direct(ผันตรง) ',min_value=0.0,max_value=10000.0,value=1000.0,step=0.1,format='%f')
+        capital_Direct  = st.sidebar.number_input('capital_Direct(เงินทุนเริ่มต้น) ',min_value=0.0,max_value=10000.0,value=1000.0,step=0.1,format='%f')
         Direct          = Run_model(capital=capital_Direct)
-        upper_Direct    = st.sidebar.number_input('upper_Direct(ผันตรง)   ',min_value=0.0,max_value=10000.0,value=100.0,step=0.1,format='%f')       
-        lowwer_Direct   = st.sidebar.number_input('lowwer_Direct(ผันตรง)  ',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')
+        upper_Direct    = st.sidebar.number_input('upper_Direct(โซนบน)   ',min_value=0.0,max_value=10000.0,value=100.0,step=0.1,format='%f')       
+        lowwer_Direct   = st.sidebar.number_input('lowwer_Direct(โซนล่าง)  ',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')
         Asset_prices    = st.number_input('Asset_prices ', min_value=lowwer_Direct ,max_value= upper_Direct ,value=50.0,step=0.1,format='%f')
         _               = Direct.Direct(upper=upper_Direct ,lowwer=lowwer_Direct , Asset_prices=Asset_prices)
         st.sidebar.text('-'*40)
