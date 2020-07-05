2c2bfabe1a34e25d5a527b88bcd55c0a585eae58
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-06 03:04:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ class Run_model :
     def __init__(self,capital):
         self.capital = capital
         
-    def inverse (self,upper=100.0 ,lowwer=0.0):
+    def inverse (self,upper=100.0 ,lowwer=0.0 , Asset_prices=0):
         x1 = lowwer ;  y1 = self.capital
         x2 = upper  ;  y2 = 0
-        Asset_prices    = st.number_input('Asset_prices', min_value=0.0 ,max_value= upper_inverse ,value=0.0,step=0.1,format='%f')
         slope,b,_,_,_ = linregress([x1,x2],[y1,y2])
         x = np.arange(x1 , x2 , 0.01)
         y = (slope * x) + b
@@ -32,10 +31,9 @@ class Run_model :
         st.write('Port_value = ' , round(Port_value , 4))
         st.write('') ; st.write('_'*40) ; st.write('')
 
-    def Direct (self,upper=100.0 ,lowwer=0.0):
+    def Direct (self,upper=100.0 ,lowwer=0.0, Asset_prices=0):
         x1 = lowwer;  y1 = 0
         x2 = upper ;  y2 = self.capital
-        Asset_prices    = st.number_input('Asset_prices', min_value=0.0 ,max_value= upper_inverse ,value=0.0,step=0.1,format='%f')
         slope,b,_,_,_ = linregress([x1,x2],[y1,y2])
         x = np.arange(x1 , x2 , 0.01)
         y = (slope * x) + b
@@ -62,13 +60,15 @@ if __name__ == '__main__':
         inverse         = Run_model(capital=capital_inverse)
         upper_inverse   = st.sidebar.number_input('upper_inverse(ผกผัน) ',min_value=0.0,max_value=10000.0,value=100.0,step=0.1,format='%f')        
         lowwer_inverse  = st.sidebar.number_input('lowwer_inverse(ผกผัน)',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')
-        _               = inverse.inverse(upper=upper_inverse ,lowwer=lowwer_inverse)
+        Asset_prices    = st.number_input('Asset_prices', min_value=0.0 ,max_value= upper_inverse ,value=0.0,step=0.1,format='%f')
+        _               = inverse.inverse(upper=upper_inverse ,lowwer=lowwer_inverse , Asset_prices=Asset_prices)
         st.sidebar.text('-'*40)
         
     if st.checkbox('Direct(ผันตรง)'):
         capital_Direct  = st.sidebar.number_input('capital_Direct(ผันตรง) ',min_value=0.0,max_value=10000.0,value=1000.0,step=0.1,format='%f')
         Direct          = Run_model(capital=capital_Direct)
         upper_Direct    = st.sidebar.number_input('upper_Direct(ผันตรง)   ',min_value=0.0,max_value=10000.0,value=100.0,step=0.1,format='%f')       
-        lowwer_Direct   = st.sidebar.number_input('lowwer_Direct(ผันตรง)  ',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')        
-        _               = Direct.Direct(upper=upper_Direct ,lowwer=lowwer_Direct)
+        lowwer_Direct   = st.sidebar.number_input('lowwer_Direct(ผันตรง)  ',min_value=0.0,max_value=10000.0,value=0.000,step=0.1,format='%f')
+        Asset_prices    = st.number_input('Asset_prices', min_value=0.0 ,max_value= upper_inverse ,value=0.0,step=0.1,format='%f')
+        _               = Direct.Direct(upper=upper_Direct ,lowwer=lowwer_Direct , Asset_prices=Asset_prices)
         st.sidebar.text('-'*40)
