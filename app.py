6fe70a919584a90c602982912acc2177506037b7
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-05 21:00:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ class Run_model :
         plt.figure(figsize=(12,8))
         plt.plot(x , y)
         st.pyplot()
-        st.write( 'จุดละ', abs(round( slope , 2)))
-        st.write('formula')
+        st.write('Formula:')
         st.write('Portvalue =', slope ,'*(Asset prices)', + b )
         
     def Direct (self,upper=100 ,lowwer=0):
@@ -30,21 +29,21 @@ class Run_model :
         plt.figure(figsize=(12,8))
         plt.plot(x , y)
         st.pyplot()
-        st.write( 'จุดละ', round( slope , 2))
+        st.write('Formula:')
         st.write('Portvalue =', slope ,'*(Asset prices)', + b )
 
 if __name__ == '__main__':
     if st.checkbox('inverse'):
-        capital_inverse = st.sidebar.number_input('capital_inverse' , 0 , 10000 , 100)
+        capital_inverse = st.sidebar.number_input('capital_inverse(ผกผัน)' , 0 , 10000 , 100)
         inverse         = Run_model(capital=capital_inverse)
-        upper_inverse   = st.sidebar.number_input('upper_inverse'   , 0 , 10000 , 100)
-        lowwer_inverse  = st.sidebar.number_input('lowwer_inverse'  , 0 , 10000 , 0)
+        upper_inverse   = st.sidebar.number_input('upper_inverse(ผันตรง)'   , 0 , 10000 , 100)
+        lowwer_inverse  = st.sidebar.number_input('lowwer_inverse(ผันตรง)'  , 0 , 10000 , 0)
         _               = inverse.inverse(upper=upper_inverse ,lowwer=lowwer_inverse)
         st.sidebar.text('-'*40)
     if st.checkbox('Direct'):
-        capital_Direct  = st.sidebar.number_input('capital_Direct' , 0 , 10000 , 100)
+        capital_Direct  = st.sidebar.number_input('capital_Direct(ผันตรง)' , 0 , 10000 , 100)
         Direct          = Run_model(capital=capital_Direct)
-        upper_Direct    = st.sidebar.number_input('upper_Direct'   , 0 , 10000 , 100)
-        lowwer_Direct   = st.sidebar.number_input('lowwer_Direct'  , 0 , 10000 , 0)
+        upper_Direct    = st.sidebar.number_input('upper_Direct(ผันตรง)'   , 0 , 10000 , 100)
+        lowwer_Direct   = st.sidebar.number_input('lowwer_Direct(ผันตรง)'  , 0 , 10000 , 0)
         _               = Direct.Direct(upper=upper_Direct ,lowwer=lowwer_Direct)
         st.sidebar.text('-'*40)
