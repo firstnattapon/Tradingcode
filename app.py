b2ac402197304e3ea47c41b4604ed1fdf0240976
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-18 22:27:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ class Run_model :
         st.write('Rebalance = ' ,'(Ratio 1 ต่อ {:.2f})'.format((abs(slope) * Asset_prices /self.capital)))
         st.write('Mean_value = ' , '(Asset:{} & Capital:{})'.format(round((upper+lowwer)/2 , 4) ,round(self.capital/2 , 4)),
                  '(Fix_Hold {:.2f}$)'.format( Asset_prices * abs(slope)))   
-        st.write('Port_value = ' , round(slope,4) ,'*(Asset_prices) +', round(b  , 4))
+        st.write('Port_value = ' , round(slope,4) , '*(Asset_prices) +', round(b  , 4))
         st.write('Port_value = ' , round(slope,4) , '*(',Asset_prices,') +' , round(b  , 4))
         st.write('Port_value = ' , round(Port_value , 4))
         st.write('') ; st.write('_'*40) ; st.write('')
@@ -124,11 +124,91 @@ if __name__ == '__main__':
         st.sidebar.text('-'*40)
 
     code = """
-v 1.20 (7/7/2020)
-- Added  Auto_prices
+class Run_model(object) :
+    def __init__(self ):
+        self.pair_data = "BTC-PERP"
+        self.timeframe = "1h"  
+        self.loop_start = dt.datetime(2020, 6 , 30  , 0, 0)
+        self.loop_end = dt.datetime(2020, 7 , 10  , 0, 0)
+        self.input  = 'rsi'
+        self.length = 30
 
-Donate
-XRP Addres: rpXTzCuXtjiPDFysxq8uNmtZBe9Xo97JbW
-XRP Tag   : 1024466261"""
+    def dataset (self):
+        self.exchange = ccxt.ftx({'apiKey': '' ,'secret': ''  , 'enableRateLimit': True }) 
+        ohlcv = self.exchange.fetch_ohlcv(self.pair_data, self.timeframe  , limit=5000)
+        ohlcv = self.exchange.convert_ohlcv_to_trading_view(ohlcv)
+        df =  pd.DataFrame(ohlcv)
+        df.t = df.t.apply(lambda  x :  datetime.fromtimestamp(x))
+        return df
+
+    @property
+    def  loop (self):
+        df =  self.dataset()
+        df = df[df.t >= self.loop_start] ; df = df[df.t <= self.loop_end]
+        df =  df.set_index(df['t']) ; df = df.drop(['t'] , axis= 1 )
+        df = df.rename(columns={"o": "open", "h": "high"  , "l": "low", "c": "close" , "v": "volume"})
+        dataset = df  ; dataset = dataset.dropna()
+        return dataset
+
+    def  represent (self):
+        df = self.loop ; df.ta.ohlc4(append=True)
+        return df
+
+    def god_returns (self):
+        god_returns = self.represent()
+        god_returns['Mk_Returntime+1']  = np.log(god_returns['OHLC4'] / god_returns['OHLC4'].shift(1))
+        god_returns['Mk_Returntime+1'] = god_returns['Mk_Returntime+1'].shift(-1)
+        god_returns['God_Buyonly'] = np.where( god_returns['Mk_Returntime+1'] > 0 ,  god_returns['Mk_Returntime+1']    , 0  )
+        god_returns['God_Sellonly'] = np.where( god_returns['Mk_Returntime+1'] < 0 ,  abs(god_returns['Mk_Returntime+1'])    , 0  )
+        god_returns['God_Buysell'] = np.where( True ,  abs(god_returns['Mk_Returntime+1'])  ,  abs(god_returns['Mk_Returntime+1'])  )
+        god_returns['Cum_Godbuyonly'] = np.cumsum(god_returns['God_Buyonly'])
+        god_returns['Cum_GodSellonly'] = np.cumsum(god_returns['God_Sellonly'])
+        god_returns['Cum_Buysell'] = np.cumsum(god_returns['God_Buyonly'])
+        god_returns['Cum_Buyhold']  = np.cumsum(god_returns['Mk_Returntime+1'])
+        god_returns = god_returns.iloc[: , -9:]
+        return god_returns
+
+    def  fx (self):
+        fx = self.represent()
+        fx['Mk_Returntime+1']  = np.log(fx['OHLC4'] / fx['OHLC4'].shift(1))
+        fx['Mk_Returntime+1'] = fx['Mk_Returntime+1'].shift(-1)
+        try: fx['F(x)'] = fx.ta(kind =self.input , length= self.length , scalar=1 , append=False)
+        except:pass
+        fx = fx.iloc[: , 5:] ; fx = fx.fillna(0)  ; fx_toaction = fx
+        fx_toaction['F(x)_Action'] = np.where( fx_toaction['F(x)'].shift(1) <  fx_toaction['F(x)'].shift(0)  , 'buy' , 'sell' )
+        fx_toaction['F(x)_BuyReturn'] = np.where(fx_toaction['F(x)_Action'] == 'buy'  , fx_toaction['Mk_Returntime+1'] ,  0)
+        fx_toaction['F(x)_CumBuyonly'] = np.cumsum(fx_toaction['F(x)_BuyReturn'])
+        fx_toaction['F(x)_SellReturn'] = np.where(fx_toaction['F(x)_Action'] == 'sell'  , -fx_toaction['Mk_Returntime+1'] ,  0)
+        fx_toaction['F(x)_CumSellonly'] = np.cumsum(fx_toaction['F(x)_SellReturn'])
+        fx_toaction['F(x)_BuySellReturn'] = np.where( fx_toaction['F(x)_Action'] == 'buy' , fx_toaction['Mk_Returntime+1'] , -fx_toaction['Mk_Returntime+1'])
+        fx_toaction['F(x)_CumBuySell'] = np.cumsum(fx_toaction['F(x)_BuySellReturn'])
+        return  fx_toaction
+
+    def  fx_scatter (self):
+        dataset = self.fx()
+        dataset['buy'] = dataset.apply(lambda x : np.where(x['F(x)_Action'] == 'buy' , x.OHLC4 , None) , axis=1)
+        dataset['sell'] =  dataset.apply(lambda x : np.where(x['F(x)_Action'] == 'sell'  , x.OHLC4 , None) , axis=1)
+        plt.figure(figsize=(12,8))
+        plt.plot(dataset.OHLC4 , color='k' , alpha=0.20 )
+        plt.plot(dataset.buy , 'o',  color='g' , alpha=0.50 )
+        plt.plot(dataset.sell , 'o', color='r' , alpha=0.50)      
+        plt.show()
+        
+    def  fx_chart (self):
+        fx_chart = self.fx()
+        plt.figure(figsize=(12,8))
+        plt.plot(fx_chart['F(x)_CumBuyonly'], color='k',  alpha=0.60 )
+        plt.plot(fx_chart['F(x)_CumSellonly'], color='g',  alpha=0.60 )
+        plt.plot(fx_chart['F(x)_CumBuySell'], color='r',  alpha=0.60 )
+        plt.show()
+
+    def god_chart (self):
+        god_chart = self.god_returns()
+        plt.figure(figsize=(12,8))
+        plt.plot(god_chart['Cum_Godbuyonly'], color='k',  alpha=0.60 )
+        plt.plot(god_chart['Cum_Godsellonly'], color='g',  alpha=0.60 )
+        plt.plot(god_chart['Cum_Buysell'], color='r',  alpha=0.60 )
+        plt.show()
+"""
     st.sidebar.code(code, language='python')
     
