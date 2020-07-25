8b44c5efee444a3ddb56545688c32fdba3e78aed
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-26 01:01:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ class Run_model :
         st.write('Port_value = ' , round(slope,4) , '*(Asset_prices) +', round(b  , 4))
         st.write('Port_value = ' , round(slope,4) , '*(',Asset_prices,') +' , round(b  , 4))
         st.write('Port_value = ' , round(Port_value , 4))
-        st.write('Cash_total = ' , round(Asset_prices * abs(slope) , 4))
+        st.write('Cash_total = ' , round(Asset_prices * abs(slope) , 4)  , '({})'.format(round(self.capital/2 , 4)) )
         st.write('Port_total = ' , round((Asset_prices * abs(slope)) + round(self.capital/2,4),4))
         st.write('') ; st.write('_'*40) ; st.write('')
      
@@ -87,14 +87,14 @@ class Run_model :
         st.write('Port_value = ' , round(slope,4) ,'*(Asset_prices) +', round(b  , 4))
         st.write('Port_value = ' , round(slope,4) , '*(',Asset_prices,') +' , round(b  , 4))
         st.write('Port_value = ' , round(Port_value , 4))
-        st.write('Cash_total = ' , round(Asset_prices * abs(slope) , 4) )
+        st.write('Cash_total = ' , round(Asset_prices * abs(slope) , 4)  , '({})'.format(round(self.capital/2 , 4)) )
         st.write('Port_total = ' , round((Asset_prices * abs(slope)) + round(self.capital/2,4),4))
         st.write('') ; st.write('_'*40) ; st.write('')
 if __name__ == '__main__':
     st.subheader('Tradingcode')  ; st.write('-'*50)
     if  st.checkbox('Inverse (ผกผัน)', value= 1):
         st.sidebar.text('-'*40)
-        capital_inverse  = st.sidebar.number_input('capital_inverse(เงินทุนเริ่มต้น)',min_value=0.0,max_value=20000.0,value=41.6 ,step=0.1,format='%f')   
+        capital_inverse  = st.sidebar.number_input('capital_inverse(เงินทุนเริ่มต้น)',min_value=0.0,max_value=20000.0,value=43.1 ,step=0.1,format='%f')   
         inverse          = Run_model(capital=capital_inverse)
         upper_inverse    = st.sidebar.number_input('upper_inverse(โซนบน)    !ต้องมากกว่า Asset_prices',min_value=0.0,max_value=30000.0,value=0.010396 ,step=0.1,format='%f')        
         lowwer_inverse   = st.sidebar.number_input('lowwer_inverse(โซนล่าง) !ต้องน้อยกว่า Asset_prices',min_value=0.0,max_value=30000.0,value=0.00000 ,step=0.1,format='%f')
@@ -111,7 +111,7 @@ if __name__ == '__main__':
         
     if  st.checkbox('Direct (ผันตรง)', value=0 ):
         st.sidebar.text('-'*40)
-        capital_Direct   = st.sidebar.number_input('capital_Direct(เงินทุนเริ่มต้น) ',min_value=0.0,max_value=20000.0,value=41.6,step=0.1,format='%f')
+        capital_Direct   = st.sidebar.number_input('capital_Direct(เงินทุนเริ่มต้น) ',min_value=0.0,max_value=20000.0,value=43.1,step=0.1,format='%f')
         Direct              = Run_model(capital=capital_Direct)
         upper_Direct    = st.sidebar.number_input('upper_Direct(โซนบน)   !ต้องมากกว่า Asset_price   ',min_value=0.0,max_value=30000.0,value=0.00787 ,step=0.1,format='%f')       
         lowwer_Direct  = st.sidebar.number_input('lowwer_Direct(โซนล่าง)!ต้องน้อยกว่า Asset_prices ',   min_value=0.0,max_value=30000.0,value=0.00000 ,step=0.1,format='%f')
