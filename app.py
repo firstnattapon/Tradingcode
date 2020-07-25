dddddb00a50e820b1b50b12a9c5b776220588e68
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-25 09:22:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,8 +109,8 @@ if __name__ == '__main__':
         st.sidebar.text('-'*40)
         capital_Direct   = st.sidebar.number_input('capital_Direct(เงินทุนเริ่มต้น) ',min_value=0.0,max_value=20000.0,value=46.0,step=0.1,format='%f')
         Direct              = Run_model(capital=capital_Direct)
-        upper_Direct    = st.sidebar.number_input('upper_Direct(โซนบน)   !ต้องมากกว่า Asset_price   ',min_value=0.0,max_value=30000.0,value=1.20 .0,step=0.1,format='%f')       
-        lowwer_Direct  = st.sidebar.number_input('lowwer_Direct(โซนล่าง)!ต้องน้อยกว่า Asset_prices ',min_value=0.0,max_value=30000.0,value=0.635,step=0.1,format='%f')
+        upper_Direct    = st.sidebar.number_input('upper_Direct(โซนบน)   !ต้องมากกว่า Asset_price   ',min_value=0.0,max_value=30000.0,value=1.20 ,step=0.1,format='%f')       
+        lowwer_Direct  = st.sidebar.number_input('lowwer_Direct(โซนล่าง)!ต้องน้อยกว่า Asset_prices ',   min_value=0.0,max_value=30000.0,value=0.635,step=0.1,format='%f')
         Auto_asset = st.checkbox('Auto_prices ',value=False )
         if Auto_asset :   
             pair_data      =  st.text_input( 'Symbol_ftx' , 'TOMO-PERP')
