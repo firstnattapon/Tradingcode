79cda17d87493f2b7cd2cd73146a17e19be68646
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-25 09:27:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ class Run_model :
         x1 = lowwer;  y1 = 0
         x2 = upper ;  y2 = self.capital
         slope,b,_,_,_ = linregress([x1,x2],[y1,y2])
-        x = np.arange(x1 , x2 , 0.05)
+        x = np.arange(x1 , x2 , 0.0010)
         y = (slope * x) + b
         plt.figure(figsize=(12,8))
         Port_value = slope *(Asset_prices) + b
